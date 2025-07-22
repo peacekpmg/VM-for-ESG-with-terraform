a0940f5bae18fe1875e3e7a9c74d9495139d7acb
peacekpmg <amarachi.onukogu@ng.kpmg.com>
2025-07-22 14:36:24 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,23 +6,37 @@ variable "resource_group_name" {
 
 # Azure region to deploy resources
 variable "location" {
-  description = "Azure location"
+  description = "Azure region for all resources"
   type        = string
   default     = "East US"
 }
 
 # Admin username for the Windows VM
 variable "vm_admin_username" {
-  description = "Admin username for VM"
+  description = "Admin username for the Windows VM"
   type        = string
 }
 
-# Admin password for the Windows VM (marked sensitive)
+# Admin password for the Windows VM
 variable "vm_admin_password" {
-  description = "Admin password for VM"
+  description = "Admin password for the Windows VM"
   type        = string
   sensitive   = true
 }
 
-# Size of the VM instance
+# VM size (SKU) for the Windows VM
 variable "vm_size" {
+  description = "Azure VM size"
+  type        = string
+  default     = "Standard_DS2_v2"
+}
+
+# Tags applied to all resources
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {
+    environment = "production"
+    project     = "esg-platform"
+  }
+}
